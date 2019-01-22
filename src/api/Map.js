bfca9114ca6ebf409a2bfde1e137e923b12fc557
Bjørn Sandvik <bjorn@mastermaps.com>
2019-01-22 11:57:52 +0100

    Simple wrapper

---
+++ src/api/Map.js
@@ -0,0 +1,17 @@
+import mapboxgl from 'mapbox-gl';
+
+const Map = class {
+    constructor(el) {
+        // TODO: create map
+    }
+
+    addLayer() {
+
+    }
+
+    removeLayer() {
+
+    }
+}
+
+export default mapboxgl;
---
+++ src/api/index.js
@@ -0,0 +1,8 @@
+/**
+ *  Wrapper around Mapbox GL JS for DHIS2 Maps
+ */
+
+import { version } from '../../package.json';
+import { Map } from './Map';
+
+export default { version };
