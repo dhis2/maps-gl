16f50357d5ef34fc028ae6f70ad60b64e8c11e39
Austin McGee <>
2019-01-22 12:00:32 +0000

    Visible basemap, sketchy functionality

--- src/api/Map.js
+++ src/api/Map.js
@@ -3,29 +3,32 @@ import mapboxgl from 'mapbox-gl';
 export class Map {
     constructor(el) {
         this.map = new mapboxgl.Map({
-            container: el,
-            style: {
-                version: 8,
-                sources: {
-                  osmLight: {
-                    type: 'raster',
-                    tiles: [
-                      '///cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png'
-                    ],
-                    tileSize: 256,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-                  },
-                },
-                layers: [{
-                    id: 'osmLight',
-                    type: 'raster',
-                    source: 'osmLight',
-                    minzoom: 0,
-                    maxzoom: 22
-                }],
+          container: el,
+          style: {
+            version: 8,
+            sources: {
+              osmLight: {
+                type: "raster",
+                tiles: [
+                  "///cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+                ],
+                tileSize: 256,
+                attribution:
+                  '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+              }
             },
-            center: [-74.50, 40], 
-            zoom: 2 
+            layers: [
+              {
+                id: "osmLight",
+                type: "raster",
+                source: "osmLight",
+                minzoom: 0,
+                maxzoom: 22
+              }
+            ]
+          },
+          center: [-74.5, 40],
+          zoom: 2
         });
     }
 
