a3df9910012873fd24577b8f6a7b6488c7618e6b
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-03-17 16:28:55 +0100

    chore: compile with babel

---
+++ babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function(api) {
+    api.cache.forever()
+
+    let presets
+
+    if (process.env.BABEL_ENV === 'modules') {
+        presets = []
+    } else {
+        presets = [
+            [
+                '@babel/preset-env',
+                {
+                    modules: 'commonjs',
+                },
+            ],
+        ]
+    }
+
+    return {
+        presets,
+        plugins: [
+            '@babel/plugin-proposal-object-rest-spread',
+            '@babel/plugin-proposal-class-properties',
+        ],
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@
  *  Wrapper around Mapbox GL JS for DHIS2 Maps
  */
 
-import { version } from '../../package.json'
 import { Map } from './Map'
 
-// export default { version };
 export default Map
