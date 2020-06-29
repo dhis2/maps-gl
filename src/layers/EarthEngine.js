5a9e2ecfa47e4bff9ba7d24fa443ba10ab3774cb
Bjorn Sandvik <bjorn@dhis2.org>
2020-06-29 21:05:53 +0200

    fix: dependency upgrades (#120)

--- src/layers/EarthEngine.js
+++ src/layers/EarthEngine.js
@@ -23,7 +23,7 @@ class EarthEngine extends Layer {
         const id = this.getId()
 
         this._ee = await getEarthEngineApi()
-        
+
         await this.setAuthToken()
 
         this._eeMap = await this.visualize(this.createImage())
@@ -31,6 +31,7 @@ class EarthEngine extends Layer {
         this.setSource(id, {
             type: 'raster',
             tileSize: 256,
+            tiles: [this._eeMap.urlFormat],
         })
 
         this.addLayer({
@@ -40,22 +41,6 @@ class EarthEngine extends Layer {
         })
 
         return this._source
-    }
-
-    // Overrides tile.loadTile function to get tile url from EE API
-    // https://issuetracker.google.com/issues/149420532
-    onAdd() {
-        const source = this.getMapGL().getSource(this.getId())
-        const loadTile = source.loadTile.bind(source)
-
-        source.loadTile = (tile, callback) => {
-            const { canonical } = tile.tileID
-            const { x, y, z } = canonical
-
-            canonical.url = () => this._ee.data.getTileUrl(this._eeMap, x, y, z)
-
-            loadTile(tile, callback)
-        }
     }
 
     // Configures client-side authentication of EE API calls by providing a OAuth2 token to use.
--- src/layers/Layer.js
+++ src/layers/Layer.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid/v4'
+import { v4 as uuid } from 'uuid'
 import bbox from '@turf/bbox'
 import { Evented } from 'mapbox-gl'
 import { addImages } from '../utils/images'
