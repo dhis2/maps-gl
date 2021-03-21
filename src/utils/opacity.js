4e81ebc1d0a5a97a99dc0e436ef4b689de10f1e4
Bjorn Sandvik <bjorn@dhis2.org>
2021-03-21 16:22:28 +0100

    fix: icon label opacity (#299)

--- src/utils/opacity.js
+++ src/utils/opacity.js
@@ -1,33 +1,36 @@
 const properties = {
-    'raster': ['raster-opacity'],
-    'point': ['circle-opacity', 'circle-stroke-opacity'],
-    'polygon': ['fill-opacity'],
-    'line': ['line-opacity'],
-    'outline': ['line-opacity'],
-    'buffer': ['fill-opacity'],
+    raster: ['raster-opacity'],
+    point: ['circle-opacity', 'circle-stroke-opacity'],
+    polygon: ['fill-opacity'],
+    line: ['line-opacity'],
+    outline: ['line-opacity'],
+    buffer: ['fill-opacity'],
     'buffer-outline': ['line-opacity'],
-    'label': ['text-opacity'],
-    'icon': ['icon-opacity'],
-    'cluster': ['circle-opacity', 'circle-stroke-opacity'],
-    'count': ['text-opacity'],
+    label: ['text-opacity'],
+    icon: ['icon-opacity', 'text-opacity'],
+    cluster: ['circle-opacity', 'circle-stroke-opacity'],
+    count: ['text-opacity'],
 }
 
-const opacityFactor =  {
-    'buffer': 0.2,
+const opacityFactor = {
+    buffer: 0.2,
     'buffer-outline': 0.2,
 }
 
-const getOpacity = (key, opacity) =>  opacity * (opacityFactor[key] || 1)
+const getOpacity = (key, opacity) => opacity * (opacityFactor[key] || 1)
 
 export const setLayersOpacity = (mapgl, id, opacity) => {
     Object.keys(properties).forEach(key => {
         const layerId = `${id}-${key}`
 
         if (mapgl.getLayer(layerId)) {
-            properties[key].forEach(property => 
-                mapgl.setPaintProperty(layerId, property, getOpacity(key, opacity))
+            properties[key].forEach(property =>
+                mapgl.setPaintProperty(
+                    layerId,
+                    property,
+                    getOpacity(key, opacity)
+                )
             )
         }
     })
 }
-
