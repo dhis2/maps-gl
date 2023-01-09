17b32cac1e9d271ef32cba789761463e519b7422
Bjorn Sandvik <bjorn@dhis2.org>
2023-01-09 17:19:59 +0100

    Revert "feat: support SVG symbols (DHIS2-14440) (#517)" (#519)
    This reverts commit 87c25b9987d0892309db1833c6c232830ae281c3.

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,67 +1,24 @@
-// Only one SVG icon size is currently suppored
-const svgWidth = 16
-const svgHeight = 16
-
-const credentials = 'include'
-
-// Add image to map sprite if not exist
-const addImage = (map, name, img) => {
-    if (!map.hasImage(name)) {
-        map.addImage(name, img)
-    }
-}
-
-// Creates image from SVG data URI
-const dataUri2image = dataUri =>
-    new Promise((resolve, reject) => {
-        const img = new Image(svgWidth, svgHeight)
-        img.onload = () => resolve(img)
-        img.onerror = reject
-        img.src = dataUri
-    })
-
-// Fetch SVG and convert to Base64 data URI
-const fetchSvg = url =>
-    fetch(url, { credentials })
-        .then(response => response.text())
-        .then(
-            svg => `data:image/svg+xml;charset=utf-8;base64,${window.btoa(svg)}`
-        )
-        .then(dataUri2image)
-
-// Load and add image to map
-const loadImage = map => url =>
-    new Promise(resolve => {
-        if (url.endsWith('.svg')) {
-            fetchSvg(url)
-                .then(img => {
-                    addImage(map, url, img)
-                    resolve(img)
-                })
-                .catch(() => {
-                    resolve()
-                })
-        } else {
-            map.loadImage(url, (error, img) => {
-                if (img) {
-                    addImage(map, url, img)
-                }
-                resolve(img)
-            })
-        }
-    })
-
 // Load and add images to map sprite
-export const addImages = async (map, images) => {
-    const result = await Promise.all(images.map(loadImage(map)))
-    const errorIndex = result.indexOf(undefined)
+export const addImages = async (map, images) =>
+    Promise.all(
+        images.map(
+            url =>
+                new Promise((resolve, reject) => {
+                    map.loadImage(url, (error, img) => {
+                        if (error) {
+                            reject(error)
+                        }
 
-    // Throws error for the first image not found
-    if (errorIndex !== -1) {
-        throw `Symbol not found: ${images[errorIndex]}`
-    }
-}
+                        if (!map.hasImage(url)) {
+                            map.addImage(url, img)
+                        }
+
+                        resolve(img)
+                    })
+                })
+        )
+    )
 
 // Include cookies for cross-origin image requests
 export const transformRequest = (url, resourceType) =>
-    resourceType === 'Image' ? { url, credentials } : null
+    resourceType === 'Image' ? { url, credentials: 'include' } : null
