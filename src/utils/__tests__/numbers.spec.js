9f9fddf046d601e296c578afb6186f8ce32cc707
Jen Jones Arnesen <jennifer@dhis2.org>
2024-06-20 15:14:03 +0200

    chore: update numbers tests

--- src/utils/__tests__/numbers.spec.js
+++ src/utils/__tests__/numbers.spec.js
@@ -8,7 +8,7 @@ import {
 } from '../numbers'
 
 describe('numbers', () => {
-    it('Should round number to x decimals', () => {
+    it('numberPrecision should round number to x decimals', () => {
         const formatNumber = numberPrecision(3)
 
         expect(formatNumber(123.342342342)).toBe(123.342)
@@ -16,21 +16,37 @@ describe('numbers', () => {
         expect(formatNumber(123.39)).toBe(123.39)
     })
 
-    it('Should return decimal precision based on value', () => {
+    it('getPrecision should return decimal precision based on value', () => {
         expect(getPrecision(542.312321312)).toBe(0)
         expect(getPrecision(78.312321312)).toBe(1)
         expect(getPrecision(8.312321312)).toBe(2)
         expect(getPrecision(0.312321312)).toBe(3)
     })
 
-    it('Should format number', () => {
-        expect(setPrecision(542.312321312)).toBe(542)
-        expect(setPrecision(78.312321312)).toBe(78.3)
-        expect(setPrecision(8.312321312)).toBe(8.31)
-        expect(setPrecision(0.312321312)).toBe(0.312)
-    })
-
     it('Should convert km to miles', () => {
         expect(kmToMiles(1)).toBe(0.621371192)
+    })
+
+    describe('setPrecision', () => {
+        it('should set precision when no "precision" value provided', () => {
+            expect(setPrecision(542.312321312)).toBe(542)
+            expect(setPrecision(78.312321312)).toBe(78.3)
+            expect(setPrecision(8.312321312)).toBe(8.31)
+            expect(setPrecision(0.312321312)).toBe(0.312)
+        })
+        it('should set the precision of a positive number correctly', () => {
+            const result = setPrecision(123.456, 2)
+            expect(result).toEqual(123.46)
+        })
+
+        it('should set the precision of a negative number correctly', () => {
+            const result = setPrecision(-123.456, 2)
+            expect(result).toEqual(-123.46)
+        })
+
+        it('should handle zero correctly', () => {
+            const result = setPrecision(0, 2)
+            expect(result).toEqual(0)
+        })
     })
 })
