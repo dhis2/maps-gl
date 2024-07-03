d33ddc8a7463b3b25704a17c72077ebef97022db
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-03 17:36:44 +0200

    chore: try to get prettier fixed

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,20 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     ...require(config.prettier),
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxSingleQuote: false,
+    arrowParens: 'avoid',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    proseWrap: 'preserve',
+    requirePragma: false,
+    insertPragma: false,
+    endOfLine: 'lf',
 }
