c0c45cd5456b07ccc39525bd6d419054923a277a
Bruno Raimbault <bruno@dhis2.org>
2026-07-29 14:42:56 +0200

    chore: sonarqube fix

--- src/layers/EarthEngine.js
+++ src/layers/EarthEngine.js
@@ -254,7 +254,7 @@ class EarthEngine extends Layer {
 
     _updateMask() {
         const mapgl = this.getMapGL()
-        const maskSource = mapgl && mapgl.getSource(`${this.getId()}-mask`)
+        const maskSource = mapgl?.getSource(`${this.getId()}-mask`)
 
         if (!maskSource) {
             return
@@ -262,13 +262,17 @@ class EarthEngine extends Layer {
 
         const { _filteredFeatureIds: filteredIds, _visibleIds: visibleIds } =
             this
+        const filteredIdSet = Array.isArray(filteredIds)
+            ? new Set(filteredIds)
+            : null
+        const visibleIdSet = Array.isArray(visibleIds)
+            ? new Set(visibleIds)
+            : null
 
         const hidden = this.getFeatures().filter(feature => {
             const id = feature.properties.id
-            const passesFilter =
-                !Array.isArray(filteredIds) || filteredIds.includes(id)
-            const passesVisible =
-                !Array.isArray(visibleIds) || visibleIds.includes(id)
+            const passesFilter = !filteredIdSet || filteredIdSet.has(id)
+            const passesVisible = !visibleIdSet || visibleIdSet.has(id)
 
             return !(passesFilter && passesVisible)
         })
